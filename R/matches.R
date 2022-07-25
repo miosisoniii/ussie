2bbfe65c808fe24d50de247f6b75e51d2086eeca
Artemio Sison <23582531+miosisoniii@users.noreply.github.com>
2022-07-25 11:41:34 -0400

    added documentation for uss_make_matches

--- R/matches.R
+++ R/matches.R
@@ -1,6 +1,16 @@
-
-
-
+#' Makes a df for league data
+#'
+#'Given a league-play data frame from {engsoccer}, returns a tibble with
+#'standardised column-names and types, e.g. `date` is a `Date`.
+#'
+#' @param data_engsoc dataframe from engsoccerdata package
+#' @param country country `character` scalar, specifies the league.
+#'
+#' @return dataframe, a tibble with columns `country`, `date`, `season`, `tier`, `home`,
+#'    `visitor`, `goals_home`, `goals_visitor`.
+#' @export
+#'
+#' @examples
 uss_make_matches <- function(data_engsoc, country) {
   result <-
     data_engsoc |>
